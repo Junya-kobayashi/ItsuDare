06f16fe86242521fb4d68c2db9a05348525f3d97
小林純也 <atsubeee.ngzk@gmail.com>
2017-09-13 15:02:07 +0900

    swift事前課題

--- ItsuDaregame/ViewController.swift
+++ ItsuDaregame/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var whenLabel: UILabel!
+    @IBOutlet var whereLabel: UILabel!
+    @IBOutlet var whoLabel: UILabel!
+    @IBOutlet var whatLabel: UILabel!
+    
+    let whenArray: [String] = ["一年前", "一週間前", "昨日", "今日"]
+    let whereArray: [String] = ["山の上で", "アメリカで", "学校で", "クラスで"]
+    let whoArray: [String] = ["僕が", "君が", "大統領が", "友達が"]
+    let whatArray: [String] = ["叫んだ", "演説した", "楽しんだ", "踊った"]
 
+    var index: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func change(){
+        whenLabel.text = whenArray[index]
+        whereLabel.text = whereArray[index]
+        whoLabel.text = whoArray[index]
+        whatLabel.text = whatArray[index]
+        
+        index = index + 1
+        
+        if index > 3 {
+            index = 0
+        }
+    }
+    
+    @IBAction func reset() {
+        whenLabel.text = "____"
+        whereLabel.text = "____"
+        whoLabel.text = "____"
+        whatLabel.text = "____"
+        
+        index = 0
+    }
+    
+    @IBAction func random(){
+        let whenIndex = Int(arc4random_uniform(4))
+        let whereIndex = Int(arc4random_uniform(4))
+        let whoIndex = Int(arc4random_uniform(4))
+        let whatIndex = Int(arc4random_uniform(4))
+        
+        print("いつ: \(whenIndex)")
+        print("どこで: \(whereIndex)")
+        print("だれが: \(whoIndex)")
+        print("何した: \(whatIndex)")
+        
+        
+        whenLabel.text = whenArray[whenIndex]
+        whereLabel.text = whereArray[whereIndex]
+        whoLabel.text = whoArray[whoIndex]
+        whatLabel.text = whatArray[whatIndex]
+    }
 
 }
 
